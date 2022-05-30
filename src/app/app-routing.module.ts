c98184f2e58acd6edea12174a8c001de556899e9
mohamedhadou157 <106554081+mohamedhadou157@users.noreply.github.com>
2022-05-31 00:30:40 +0100

    add redirect home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './components/home/home.component';
 import { ListClientComponent } from './components/home/client/list-client/list-client.component';
 
 const routes: Routes = [
+{path:'', redirectTo:'/home', pathMatch: 'full'},
   {
     path: 'home',
     component: HomeComponent,
